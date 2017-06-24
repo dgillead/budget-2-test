a24e4f495a012f28c78b1673ad5b6b42d5865b1e
Darnell Gillead <dlgillead@gmail.com>
2017-06-24 18:31:04 -0500

    why

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,10 @@
 class Activity < ActiveRecord::Base
   belongs_to :budget
   has_many :categories
+
+  class << self
+    def new_activity(amount, name, category)
+      Activity.create(:amount => amount, :name => name, :category_name => category)
+    end
+  end
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -9,15 +9,19 @@ class Budget < ActiveRecord::Base
     end
 
     def set_budget_limit(limit_amount)
+      # binding.pry
       Budget.create(:budget_limit => limit_amount)
     end
 
-    def change_budget_limit(limit_amount)
-      Budget.where('budget_limit IS NOT NULL').update(:budget_limit => limit_amount)
-    end
+    # def change_budget_limit(limit_amount)
+    #   @@total_limit = @@total_limit.to_i
+    #   @@total_limit -= limit_amount.to_i
+    #   # binding.pry
+    #   Budget.where('budget_limit IS NOT NULL').update(:budget_limit => @@total_limit)
+    # end
 
     def see_current_limit
-      Budget.pluck(:budget_limit).first
+      Budget.pluck(:budget_limit)
     end
   end
 end
